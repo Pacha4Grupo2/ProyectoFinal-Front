ff47cccdb5d444f4bf1b8f525f794e7bb68d8817
Bryan Vera <bveracachay@gmail.com>
2020-09-14 13:35:13 -0500

    se realizo algunos cambio en sass

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,46 @@
 <template>
-  <div class="app">
-    <ul>
-      <li>
-        <router-link to="/auth/login">LOGIN</router-link>
-      </li>
-      <li>
-        <router-link to="/auth/register">REGISTER</router-link>
-      </li>
-      <li>
-        <router-link to="/">HOME</router-link>
-      </li>
-      <li>
-        <router-link to="/detalle">DETALLE HOME</router-link>
-      </li>
-      <li>
-        <router-link to="/pago/checkout">PAGO / CHECKOUT</router-link>
-      </li>
-      <li>
-        <router-link to="/pago/tarjeta-de-credito">PAGO / TARJETA DE CREDITO</router-link>
-      </li>
-      <li>
-        <router-link to="/pago/tarjeta-de-debito">PAGO / TARJETA DE DEBITO</router-link>
-      </li>
-      <li>
-        <router-link to="/pago/efectivo">PAGO / PAGO EFECTIVO</router-link>
-      </li>
-      <li>
-        <router-link to="/pago/resumen">PAGO / COMPRA RESUMEN</router-link>
-      </li>
-    </ul>
-    <router-view></router-view>
-  </div>
+	<div class="app">
+		<ul>
+			<li>
+				<router-link to="/auth/login">LOGIN</router-link>
+			</li>
+			<li>
+				<router-link to="/auth/register">REGISTER</router-link>
+			</li>
+			<li>
+				<router-link to="/">HOME</router-link>
+			</li>
+			<li>
+				<router-link to="/detalle">DETALLE HOME</router-link>
+			</li>
+			<li>
+				<router-link to="/pago/checkout">PAGO / CHECKOUT</router-link>
+			</li>
+			<li>
+				<router-link to="/pago/tarjeta-de-credito"
+					>PAGO / TARJETA DE CREDITO</router-link
+				>
+			</li>
+			<li>
+				<router-link to="/pago/tarjeta-de-debito"
+					>PAGO / TARJETA DE DEBITO</router-link
+				>
+			</li>
+			<li>
+				<router-link to="/pago/efectivo">PAGO / PAGO EFECTIVO</router-link>
+			</li>
+			<li>
+				<router-link to="/pago/resumen">PAGO / COMPRA RESUMEN</router-link>
+			</li>
+		</ul>
+		<router-view></router-view>
+	</div>
 </template>
 <script>
 export default {
-  name: "App",
+	name: 'App',
 };
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
+@import '@/assets/styles/main.scss';
 </style>
