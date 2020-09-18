2f904372e9153f74cec0d11350825c3f98e9823e
Dede1421 <mauricioandre91@gmail.com>
2020-09-17 19:58:35 -0500

    Loggin y register casi finalizado

--- src/views/Auth/AuthLogin.vue
+++ src/views/Auth/AuthLogin.vue
@@ -1,7 +1,14 @@
 <template>
-  <div class="auth-login">
-    <!-- <h2>PAGINA LOGIN</h2> -->
-  </div>
+  <form action="#">
+    <div>
+      <input type="email" placeholder="Correo electronico" required />
+      <input type="password" placeholder="Contraseña" required/>
+    </div>
+    <div class="enter">
+      <button class="button">Ingresar</button>
+      <a href>¿Olvidaste tu Contraseña?</a>
+    </div>
+  </form>
 </template>
 
 <script>
@@ -10,5 +17,47 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.button {
+  border-style: none;
+  padding: 15px;
+  width: 100%;
+  font-weight: bold;
+  font-size: 1.5rem;
+  outline: none;
+  margin-bottom: 10px;
+}
+
+form {
+  padding: 20px;
+  height: 60%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  p {
+    color: #000;
+    padding-bottom: 20px;
+  }
+}
+
+input {
+  padding: 15px;
+  width: 88%;
+  outline: none;
+  margin-bottom: 20px;
+  border: 1px solid rgba(0, 0, 0, 0.5);
+  border-radius: 5px;
+}
+
+.enter {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  // padding: 20px;
+}
+
+a {
+  font-weight: bold;
+  text-align: center;
+}
 </style>
--- src/views/Auth/AuthRegister.vue
+++ src/views/Auth/AuthRegister.vue
@@ -1,7 +1,19 @@
 <template>
-  <div class="auth-register">
-    <!-- <h2>PAGINA REGISTER</h2> -->
-  </div>
+  <form action="#">
+    <div>
+      <input type="text" placeholder="Nombre y Apellidos" required />
+      <input type="email" placeholder="Correo electrónico" required />
+      <input type="password" placeholder="Contraseña" required />
+      <input type="password" placeholder="Repite contraseña" required />
+      <div class="checkbox-container">
+        <input class="checkbox" type="checkbox" name="accept" required />
+        <label>Acepto Términos y condiciones</label>
+      </div>
+    </div>
+    <!-- <div class="enter"> -->
+    <button class="button">Ingresar</button>
+    <!-- </div> -->
+  </form>
 </template>
 
 <script>
@@ -10,5 +22,52 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.button {
+  border-style: none;
+  padding: 15px;
+  font-weight: bold;
+  font-size: 1.5rem;
+  outline: none;
+  margin-bottom: 10px;
+}
+
+form {
+  padding: 20px;
+  height: 60%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  p {
+    color: #000;
+    padding-bottom: 20px;
+  }
+}
+
+input {
+  padding: 15px;
+  width: 88%;
+  outline: none;
+  margin-bottom: 20px;
+  border: 1px solid rgba(0, 0, 0, 0.5);
+  border-radius: 5px;
+}
+
+.checkbox-container {
+  display: flex;
+  align-items: center;
+}
+
+.checkbox {
+  width: 5%;
+  margin: 0px;
+  cursor: pointer;
+}
+
+.enter {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+}
 </style>
