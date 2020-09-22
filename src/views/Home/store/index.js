a336e2ecc151ac43e2266b6fa8259214657f0026
Bryan Vera <bveracachay@gmail.com>
2020-09-21 20:04:11 -0500

    se agrego boton agregar y se arreglo las cards

--- src/views/Home/store/index.js
+++ src/views/Home/store/index.js
@@ -1,46 +1,47 @@
 export default {
-    namespaced: true,
-    state: {
-        products: [],
-        errorLoaded: false,
-        modal: false,
-        backColor: false,
-    },
-    mutations: {
-        SET_PRODUCTS(state, products) {
-            state.products = products;
-        },
-        SET_ERROR_LOADED(state, stateError) {
-            state.errorLoaded = stateError;
-        },
-        MODALON(state) {
-            state.modal = !state.modal;
-            state.backColor = !state.backColor;
-        },
-        BACKCOLOR(state) {
-            state.backColor = !state.backColor;
-        },
-    },
-    actions: {
-        async getProducts({ commit }) {
-            try {
-                const products = await fetch(
-                    'https://backendhackatonfinal.herokuapp.com/producto/'
-                ).then((response) => response.json());
-                console.log('responsive', products);
-                commit('SET_ERROR_LOADED', false);
-                commit('SET_PRODUCTS', products);
-            } catch (error) {
-                commit('SET_ERROR_LOADED', true);
-            }
-        },
-        getModal({ commit }) {
-            // commit('BACKCOLOR');
-            commit('MODALON');
-        },
-        getModalB({ commit }) {
-            commit('MODALON');
-        },
-    },
-    modules: {},
+	namespaced: true,
+	state: {
+		products: [],
+		errorLoaded: false,
+		modal: false,
+		backColor: false,
+		carrito: [],
+	},
+	mutations: {
+		SET_PRODUCTS(state, products) {
+			state.products = products;
+		},
+		SET_ERROR_LOADED(state, stateError) {
+			state.errorLoaded = stateError;
+		},
+		MODALON(state) {
+			state.modal = !state.modal;
+			state.backColor = !state.backColor;
+		},
+		BACKCOLOR(state) {
+			state.backColor = !state.backColor;
+		},
+	},
+	actions: {
+		async getProducts({ commit }) {
+			try {
+				const products = await fetch(
+					'https://backendhackatonfinal.herokuapp.com/producto/'
+				).then((response) => response.json());
+				console.log('responsive', products);
+				commit('SET_ERROR_LOADED', false);
+				commit('SET_PRODUCTS', products);
+			} catch (error) {
+				commit('SET_ERROR_LOADED', true);
+			}
+		},
+		getModal({ commit }) {
+			// commit('BACKCOLOR');
+			commit('MODALON');
+		},
+		getModalB({ commit }) {
+			commit('MODALON');
+		},
+	},
+	modules: {},
 };
