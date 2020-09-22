272630bf7230560ca7f9d000e1b2f88e2d98093c
Bryan Vera <bveracachay@gmail.com>
2020-09-22 01:09:01 -0500

    se realizo el merge con andre y se solucion el problema con el merge anterior

--- src/views/Auth/AuthLogin.vue
+++ src/views/Auth/AuthLogin.vue
@@ -1,29 +1,30 @@
 <template>
-  <div class="formLogin-content">
-    <SocialMediaDiv description1="iniciar sesión" description2="iniciar sesión"/>
-    <form class="formAuth-login" action="#">
-      <div>
-        <input type="email" placeholder="Correo electronico" required />
-        <input type="password" placeholder="Contraseña" required />
-      </div>
-      <div class="enterA">
-        <button class="button btn-enterA">Ingresar</button>
-        <a href="#" class="linkA">¿Olvidaste tu Contraseña?</a>
-      </div>
-    </form>
-  </div>
+	<div class="formLogin-content">
+		<SocialMediaDiv
+			description1="iniciar sesión"
+			description2="iniciar sesión"
+		/>
+		<form class="formAuth-login" action="#">
+			<div>
+				<input type="email" placeholder="Correo electronico" required />
+				<input type="password" placeholder="Contraseña" required />
+			</div>
+			<div class="enterA">
+				<button class="button btn-enterA">Ingresar</button>
+				<a href="#" class="linkA">¿Olvidaste tu Contraseña?</a>
+			</div>
+		</form>
+	</div>
 </template>
 
 <script>
-import SocialMediaDiv from "./components/SocialMediaDiv";
+import SocialMediaDiv from './components/SocialMediaDiv';
 export default {
-  name: "AuthLogin",
-  components: {
-    SocialMediaDiv,
-  },
+	name: 'AuthLogin',
+	components: {
+		SocialMediaDiv,
+	},
 };
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
