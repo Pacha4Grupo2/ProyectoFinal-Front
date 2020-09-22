b6a679e05c1694610ae4422fa620b13078252ee9
Ricardo Nique <ricardo.19.0845@gmail.com>
2020-09-21 19:49:12 -0500

    agregando responsive design para payments

--- src/views/PaymentGateway/Checkout.vue
+++ src/views/PaymentGateway/Checkout.vue
@@ -20,32 +20,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.view-checkout h2 {
-	/*Tipografía*/
-	font-family: 'Poppins';
-	color: #000425;
-	font-size: 1.9rem;
-}
-
-.view-checkout button {
-	/*Box-model*/
-	padding: 15px 120px;
-	margin-right: 5px;
-	border: #5640ff;
-	font-weight: bold;
-	width: 100%;
-}
-
-.checkout {
-	padding: 20 20 20 20;
-}
-.sectionprice1 {
-	padding: 50px 30px 50px 30px;
-	max-width: 70%;
-	border: solid 1px rgb(86, 88, 86);
-}
-.view-checkout h4 {
-	margin-bottom: 25px;
-	font-size: 1.6rem;
-}
+@import '@/assets/styles/_checkout.scss';
 </style>
