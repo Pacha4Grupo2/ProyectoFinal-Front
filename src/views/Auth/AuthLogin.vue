c76264c11d389c7b3f8014459baca2eb01099a08
Dede1421 <mauricioandre91@gmail.com>
2020-09-21 19:11:20 -0500

    RWD Login&Register

--- src/views/Auth/AuthLogin.vue
+++ src/views/Auth/AuthLogin.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="formLogin-content">
     <SocialMediaDiv description1="iniciar sesión" description2="iniciar sesión"/>
-    <form action="#">
+    <form class="formAuth-login" action="#">
       <div>
         <input type="email" placeholder="Correo electronico" required />
         <input type="password" placeholder="Contraseña" required />
       </div>
-      <div class="enter">
-        <button class="button btn-enter">Ingresar</button>
-        <a href>¿Olvidaste tu Contraseña?</a>
+      <div class="enterA">
+        <button class="button btn-enterA">Ingresar</button>
+        <a href="#" class="linkA">¿Olvidaste tu Contraseña?</a>
       </div>
     </form>
   </div>
--- src/views/Auth/AuthRegister.vue
+++ src/views/Auth/AuthRegister.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="formRegister-content">
     <SocialMediaDiv description1="registrarte" description2="registrarte" />
-    <form action="#">
+    <form class="formAuth-register" action="#">
       <div>
         <input type="text" placeholder="Nombre y Apellidos" required />
         <input type="email" placeholder="Correo electrónico" required />
         <input type="password" placeholder="Contraseña" required />
         <input type="password" placeholder="Repite contraseña" required />
-        <div class="checkbox-container">
-          <input class="checkbox" type="checkbox" name="accept" required />
+        <div class="checkboxA-container">
+          <input class="checkboxA" type="checkbox" name="accept" required />
           <label>Acepto Términos y condiciones</label>
         </div>
       </div>
-      <button class="button btn-register">Registrarse</button>
+      <button class="button btn-registerA">Registrarse</button>
     </form>
   </div>
 </template>
