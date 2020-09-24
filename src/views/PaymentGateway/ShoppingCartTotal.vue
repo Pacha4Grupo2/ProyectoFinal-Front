65e1aecbaec22898ea4686f6ad475b12f4ed133c
sebastian yabiku <sebastian.yabiku@pachaqtec.edu.pe>
2020-09-23 21:00:38 -0500

    it added changes

--- src/views/PaymentGateway/ShoppingCartTotal.vue
+++ src/views/PaymentGateway/ShoppingCartTotal.vue
@@ -1,66 +1,66 @@
 <template>
-	<div class="view-shopping-cart-total">
-		<section class="section2">
-			<div class="section2-container d-flex flex-column">
-				<div class="section2-price d-flex justify-between">
-					<h4>Subtotal</h4>
-					<h4>S/. {{ totalPrice }}</h4>
-				</div>
-				<input type="text" placeholder="Agrega un código de descuento" />
-				<button class="button">Continuar</button>
-			</div>
-		</section>
-	</div>
+  <div class="view-shopping-cart-total">
+    <section class="section2">
+      <div class="section2-container d-flex flex-column">
+        <div class="section2-price d-flex justify-between">
+          <h4>Subtotal</h4>
+          <h4>S/. {{ totalPrice }}</h4>
+        </div>
+        <input type="text" placeholder="Agrega un código de descuento" />
+        <button class="button">Continuar</button>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex';
+import { mapState, mapActions } from "vuex";
 export default {
-	name: 'ShoppingCartTotal',
-	computed: {
-		...mapState({
-			totalPrice: (state) => state.HomeStore.tatalPrice,
-		}),
-	},
+  name: "ShoppingCartTotal",
+  computed: {
+    ...mapState({
+      totalPrice: (state) => state.HomeStore.tatalPrice,
+    }),
+  },
 
-	methods: {
-		...mapActions({
-			getPriceTotal: 'HomeStore/getPriceTotal',
-		}),
-	},
-	mounted() {
-		this.getPriceTotal();
-	},
+  methods: {
+    ...mapActions({
+      getPriceTotal: "HomeStore/getPriceTotal",
+    }),
+  },
+  mounted() {
+    this.getPriceTotal();
+  },
 };
 </script>
 
 <style scoped>
 .view-shopping-cart-total input {
-	/* Box-model */
-	padding: 10px 10px;
-	border-radius: 4px;
-	margin-bottom: 30px;
-	border-color: darkgrey;
-	width: 93%;
+  /* Box-model */
+  padding: 10px 10px;
+  border-radius: 4px;
+  margin-bottom: 30px;
+  border-color: darkgrey;
+  width: 93%;
 
-	/*Tipografía*/
-	font-family: 'roboto';
-	font-size: 1rem;
-	font-weight: bold;
+  /*Tipografía*/
+  font-family: "roboto";
+  font-size: 1rem;
+  font-weight: bold;
 }
 .section2-container {
-	border: solid 1px rgb(86, 88, 86);
-	padding: 47px 35px 55px 35px;
+  border: solid 1px rgb(86, 88, 86);
+  padding: 47px 35px 55px 35px;
 }
 .section2-price h4 {
-	color: #5640ff;
-	font-weight: bold;
-	font-size: 2.3rem;
+  color: #5640ff;
+  font-weight: bold;
+  font-size: 2.3rem;
 }
 .section2-price {
-	margin-bottom: 15px;
+  margin-bottom: 15px;
 }
 .button {
-	padding: 10px 0;
+  padding: 10px 0;
 }
 </style>
--- src/views/PaymentGateway/router.js
+++ src/views/PaymentGateway/router.js
@@ -1,50 +1,44 @@
 export default {
-    path: '/pago',
-    name: 'pago',
-    component: () =>
-        import ('./PaymentGatewayLayout.vue'),
-    children: [{
-            path: 'checkout',
-            name: 'checkout',
-            component: () =>
-                import ('./Checkout.vue'),
+  path: "/pago",
+  name: "pago",
+  component: () => import("./PaymentGatewayLayout.vue"),
+  children: [
+    {
+      path: "checkout",
+      name: "checkout",
+      component: () => import("./Checkout.vue"),
+    },
+    {
+      path: "pasarela",
+      name: "pasarela",
+      component: () => import("./Pasarela.vue"),
+      children: [
+        {
+          path: "tarjeta-de-credito",
+          name: "pago-credito",
+          component: () => import("./CreditCard.vue"),
         },
         {
-            path: 'pasarela',
-            name: 'pasarela',
-            component: () =>
-                import ('./Pasarela.vue'),
-            children: [{
-                    path: 'tarjeta-de-credito',
-                    name: 'pago-credito',
-                    component: () =>
-                        import ('./CreditCard.vue'),
-                },
-                {
-                    path: 'tarjeta-de-debito',
-                    name: 'pago-debito',
-                    component: () =>
-                        import ('./DebitCard.vue'),
-                },
-                {
-                    path: 'efectivo',
-                    name: 'pago-efectivo',
-                    component: () =>
-                        import ('./CashPayment.vue'),
-                },
-            ],
+          path: "tarjeta-de-debito",
+          name: "pago-debito",
+          component: () => import("./DebitCard.vue"),
         },
         {
-            path: 'resumen',
-            name: 'pago-resumen',
-            component: () =>
-                import ('./PaymentSummary.vue'),
+          path: "efectivo",
+          name: "pago-efectivo",
+          component: () => import("./CashPayment.vue"),
         },
-        {
-            path: 'carrito-de-compras',
-            name: 'resumen-carrito-de-compras',
-            component: () =>
-                import ('./ShoppingCartMain.vue'),
-        },
-    ],
+      ],
+    },
+    {
+      path: "resumen",
+      name: "pago-resumen",
+      component: () => import("./PaymentSummary.vue"),
+    },
+    {
+      path: "carrito-de-compras",
+      name: "resumen-carrito-de-compras",
+      component: () => import("./ShoppingCartMain.vue"),
+    },
+  ],
 };
