c7cd070dfe8913e47f2860857b1e4dc1032e0ec1
Bryan Vera <bveracachay@gmail.com>
2020-09-25 10:46:00 -0500

    se acepta el merge de mery

--- src/views/PaymentGateway/ShoppingCart.vue
+++ src/views/PaymentGateway/ShoppingCart.vue
@@ -1,167 +1,63 @@
 <template>
-<<<<<<< HEAD
-  <div class="view-shopping-cart">
-    <div class="d-flex">
-      <section class="section1">
-        <article v-for="producto in totalProducts" :key="producto.id">
-          <img :src="producto.imagen_card" />
-          <div class="d-flex flex-column justify-between">
-            <div>
-              <h2>{{ producto.nombre }}</h2>
-              <p>Dto. {{ producto.descuento }}%</p>
-            </div>
-            <button class="btn-eliminar" @click="confirmDelete(producto.id)">
-              Eliminar
-            </button>
-          </div>
-          <div>
-            <h3>S/. {{ producto.precio }}</h3>
-          </div>
-        </article>
-      </section>
-    </div>
-  </div>
-=======
 	<div class="view-shopping-cart">
 		<div class="shopping-cart-detail">
+			<article
+				class="shopping-cart-detail__container"
+				v-for="producto in totalProducts"
+				:key="producto.id"
+			>
+				<img :src="producto.imagen_card" />
 
-				<article class="shopping-cart-detail__container" v-for="producto in totalProducts" :key="producto.id">
-					<img :src="producto.imagen_card" />
-
-					<div class="shopping-cart-detail__container__description d-flex flex-column justify-between">
-						<div class="shopping-cart-detail__container__description__title">
-							<h2>{{ producto.nombre }}</h2>
-							<p>Dto. {{ producto.descuento }}%</p>
-						</div>
-						<button class="btn-eliminar" @click="eliminar(producto.id)">
-							Eliminar
-						</button>
+				<div
+					class="shopping-cart-detail__container__description d-flex flex-column justify-between"
+				>
+					<div class="shopping-cart-detail__container__description__title">
+						<h2>{{ producto.nombre }}</h2>
+						<p>Dto. {{ producto.descuento }}%</p>
 					</div>
+					<button class="btn-eliminar" @click="eliminar(producto.id)">
+						Eliminar
+					</button>
+				</div>
 
-					<div class="shopping-cart-detail__container__price">
-						<h3>S/. {{ producto.precio }}</h3>
-					</div>
-				</article>
-
+				<div class="shopping-cart-detail__container__price">
+					<h3>S/. {{ producto.precio }}</h3>
+				</div>
+			</article>
 		</div>
 	</div>
->>>>>>> 1f392bf2c0cfd9c1f935d4ffaed3b40396b6efda
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
+import { mapState, mapActions } from 'vuex';
 export default {
-  name: "ShoppingCart",
-  computed: {
-    ...mapState({
-      totalProducts: (state) => state.HomeStore.totalProducts,
-    }),
-  },
-  methods: {
-    ...mapActions({
-      eliminar: "HomeStore/eliminar",
-    }),
-    confirmDelete(id) {
-      this.$swal({
-        title: "Deseas eliminar este producto?",
-        showDenyButton: true,
+	name: 'ShoppingCart',
+	computed: {
+		...mapState({
+			totalProducts: (state) => state.HomeStore.totalProducts,
+		}),
+	},
+	methods: {
+		...mapActions({
+			eliminar: 'HomeStore/eliminar',
+		}),
+		confirmDelete(id) {
+			this.$swal({
+				title: 'Deseas eliminar este producto?',
+				showDenyButton: true,
 
-        confirmButtonText: `SI`,
-        denyButtonText: `NO`,
-      }).then((result) => {
-        if (result.isConfirmed) {
-          this.eliminar(id);
-        }
-      });
-    },
-  },
+				confirmButtonText: `SI`,
+				denyButtonText: `NO`,
+			}).then((result) => {
+				if (result.isConfirmed) {
+					this.eliminar(id);
+				}
+			});
+		},
+	},
 };
 </script>
 
-<<<<<<< HEAD
-<style scoped>
-.btn-eliminar {
-  border-radius: 10px;
-  cursor: pointer;
-  background-color: transparent;
-  font-family: "Roboto";
-  color: #5640ff;
-  display: inline-block;
-  text-align: left;
-  border: none;
-  outline: none;
-  text-decoration-line: underline;
-  font-weight: bold;
-}
-.view-shopping-cart h1 {
-  /* Box-model */
-  margin-bottom: 15px;
-  margin-left: 20px;
-  /*Tipografía*/
-  font-family: "Poppins";
-  color: #000425;
-  font-size: 1.6rem;
-}
-.view-shopping-cart h2 {
-  /* Box-model */
-  margin-bottom: 15px;
-  /*Tipografía*/
-  font-family: "Poppins";
-  color: #222222;
-  font-size: 1.3rem;
-  font-weight: bold;
-}
-.view-shopping-cart h3 {
-  /* Box-model */
-  margin-bottom: 15px;
-  /*Tipografía*/
-  font-family: "Poppins";
-  color: #222222;
-  font-size: 1.5rem;
-  font-weight: bold;
-  text-align: end;
-}
-.view-shopping-cart section,
-.view-shopping-cart article {
-  margin-bottom: 15px;
-}
-.view-shopping-cart article {
-  display: grid;
-  grid-template-columns: 0.23fr 0.6fr 0.2fr;
-}
-.view-shopping-cart img {
-  width: 90%;
-  height: 110px;
-  object-fit: cover;
-  border-radius: 7px;
-}
-.view-shopping-cart p,
-.view-shopping-cart a {
-  color: #5640ff;
-  font-weight: bold;
-  font-size: 1.1rem;
-}
-.view-shopping-cart input {
-  /* Box-model */
-  padding: 10px 10px;
-  border-radius: 4px;
-  margin-bottom: 30px;
-  border-color: darkgrey;
-  width: 93%;
-
-  /*Tipografía*/
-  font-family: "roboto";
-  font-size: 1rem;
-  font-weight: bold;
-}
-.view-shopping-cart {
-  padding: 15px 50px 0 40px;
-}
-.shopping-cart-title {
-  margin-bottom: 28px;
-}
-=======
-<style lang='scss'>
+<style lang="scss">
 @import '@/assets/styles/_shoppingCartDetail.scss';
->>>>>>> 1f392bf2c0cfd9c1f935d4ffaed3b40396b6efda
 </style>
--- src/views/PaymentGateway/ShoppingCartTotal.vue
+++ src/views/PaymentGateway/ShoppingCartTotal.vue
@@ -1,18 +1,4 @@
 <template>
-<<<<<<< HEAD
-  <div class="view-shopping-cart-total">
-    <section class="section2">
-      <div class="section2-container d-flex flex-column">
-        <div class="section2-price d-flex justify-between">
-          <h4>Subtotal</h4>
-          <h4>S/. {{ totalPrice }}</h4>
-        </div>
-        <input type="text" placeholder="Agrega un código de descuento" />
-        <button class="button">Continuar</button>
-      </div>
-    </section>
-  </div>
-=======
 	<div class="view-shopping-cart-total">
 		<div class="section2-container d-flex flex-column">
 			<div class="section2-price d-flex justify-between">
@@ -21,68 +7,34 @@
 			</div>
 			<div class="dsct-container">
 				<input type="text" placeholder="Agrega un código de descuento" />
-				<img src="@/assets/images/enviar.png">				
-			</div>	
+				<img src="@/assets/images/enviar.png" />
+			</div>
 			<button class="button">Continuar</button>
 		</div>
 	</div>
->>>>>>> 1f392bf2c0cfd9c1f935d4ffaed3b40396b6efda
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
+import { mapState, mapActions } from 'vuex';
 export default {
-  name: "ShoppingCartTotal",
-  computed: {
-    ...mapState({
-      totalPrice: (state) => state.HomeStore.tatalPrice,
-    }),
-  },
+	name: 'ShoppingCartTotal',
+	computed: {
+		...mapState({
+			totalPrice: (state) => state.HomeStore.tatalPrice,
+		}),
+	},
 
-  methods: {
-    ...mapActions({
-      getPriceTotal: "HomeStore/getPriceTotal",
-    }),
-  },
-  mounted() {
-    this.getPriceTotal();
-  },
+	methods: {
+		...mapActions({
+			getPriceTotal: 'HomeStore/getPriceTotal',
+		}),
+	},
+	mounted() {
+		this.getPriceTotal();
+	},
 };
 </script>
 
-<<<<<<< HEAD
-<style scoped>
-.view-shopping-cart-total input {
-  /* Box-model */
-  padding: 10px 10px;
-  border-radius: 4px;
-  margin-bottom: 30px;
-  border-color: darkgrey;
-  width: 93%;
-
-  /*Tipografía*/
-  font-family: "roboto";
-  font-size: 1rem;
-  font-weight: bold;
-}
-.section2-container {
-  border: solid 1px rgb(86, 88, 86);
-  padding: 47px 35px 55px 35px;
-}
-.section2-price h4 {
-  color: #5640ff;
-  font-weight: bold;
-  font-size: 2.3rem;
-}
-.section2-price {
-  margin-bottom: 15px;
-}
-.button {
-  padding: 10px 0;
-}
-=======
-<style lang='scss'>
+<style lang="scss">
 @import '@/assets/styles/_shoppingCartTotal.scss';
-
->>>>>>> 1f392bf2c0cfd9c1f935d4ffaed3b40396b6efda
 </style>
