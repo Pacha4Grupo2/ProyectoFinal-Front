02bfe8f1378d1d9131636066610176477558836e
Bryan Vera <bveracachay@gmail.com>
2020-09-26 13:44:30 -0500

    se agregar usuario cuando esta logeado

--- src/views/Auth/AuthUserData.vue
+++ src/views/Auth/AuthUserData.vue
@@ -1,24 +1,22 @@
 <template>
-    <div class="formLogin-content">
-      <div>
-        <h3>{{user.username}}</h3>
-      </div>
-    </div>
+	<div class="formLogin-content">
+		<div>
+			<h3>{{ user.username }}</h3>
+		</div>
+	</div>
 </template>
 
 <script>
 import { mapState } from 'vuex';
 
 export default {
-    name: "AuthUserData",
-    methods:{
-      ...mapState({
-        isLogin: (state) => state.AuthStore.isLogin,
-        user: (state) => state.AuthStore.user,
-      }),
-  }
+	name: 'AuthUserData',
+	computed: {
+		...mapState({
+			user: (state) => state.AuthStore.user,
+		}),
+	},
+	methods: {},
 };
 </script>
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
--- src/views/Auth/store/index.js
+++ src/views/Auth/store/index.js
@@ -1,79 +1,78 @@
-import router from "@/router/index";
+import router from '@/router/index';
 
 export default {
-  namespaced: true,
-  state: {
-    userToken: {},
-    isLogin: false,
-    errorLoaded: false,
-    user:{}
-  },
-  mutations: {
-    SET_USER_TOKEN(state, userToken) { // { nombre: "Andre", email: "",id: 12 }
-      state.userToken = userToken;
+    namespaced: true,
+    state: {
+        userToken: {},
+        isLogin: false,
+        errorLoaded: false,
+        user: {},
     },
-    SET_IS_LOGIN(state, isLogin) {
-      state.isLogin = true;
-      state.user= isLogin;
+    mutations: {
+        SET_USER_TOKEN(state, userToken) {
+            // { nombre: "Andre", email: "",id: 12 }
+            state.userToken = userToken;
+        },
+        SET_IS_LOGIN(state, user) {
+            state.isLogin = true;
+            console.log(state.isLogin);
+            state.user = user;
+        },
+        SET_ERROR_LOADED(state, stateError) {
+            state.errorLoaded = stateError;
+        },
     },
-    SET_ERROR_LOADED(state, stateError) {
-      state.errorLoaded = stateError;
-    },
-  },
-  actions: {
-    async postLogin({ commit }, payload) {
-      console.log("api logueo", payload);
-      try {
-        const form = new FormData()
-        form.append("username", payload.username)
-        form.append("password", payload.password)
+    actions: {
+        async postLogin({ commit }, payload) {
+            console.log('api logueo', payload);
+            try {
+                const form = new FormData();
+                form.append('username', payload.username);
+                form.append('password', payload.password);
 
-        
-        const loginResponse = await fetch(
-          "https://backendhackatonfinal.herokuapp.com/api/token/",
-          {
-            method: "POST",
-            body: form,
-          }
-        )
-        const bodyResponse = await loginResponse.json();
-        
-        console.log(loginResponse)
-        if(loginResponse.status !== 200) { // no ok
-          alert(bodyResponse.detail)
-        } else {
-          bodyResponse.access
+                const loginResponse = await fetch(
+                    'https://backendhackatonfinal.herokuapp.com/api/token/', {
+                        method: 'POST',
+                        body: form,
+                    }
+                );
+                const bodyResponse = await loginResponse.json();
 
-          // alert(`Bienvenido ${usuario.nombre}`);
-          commit("SET_ERROR_LOADED", false);
-          commit("SET_USER_TOKEN", bodyResponse.access);
-          commit("SET_IS_LOGIN", loginResponse);
-          router.push("/");
-        }
-      } catch (e) {
-        commit("SET_ERROR_LOADED", true);
-      }
-    },
-    async postRegister({ commit }, payload) {
-      try {
-        const registerResponse = await fetch(
-          "https://backendhackatonfinal.herokuapp.com/cliente/",
-          {
-            method: "POST",
-            body: JSON.stringify(payload),
-            headers: {
-              "content-Type": "application/json",
-            },
-          }
-        ).then((response) => response.json());
-        commit("SET_ERROR_LOADED", false);
-        alert(`${registerResponse.nombre} se ha registado satisfactoriamente`);
-        router.push("/auth/login");
+                console.log(loginResponse);
+                if (loginResponse.status !== 200) {
+                    // no ok
+                    alert(bodyResponse.detail);
+                } else {
+                    bodyResponse.access;
 
-      } catch (e) {
-        commit("SET_ERROR_LOADED", true);
-      }
+                    // alert(`Bienvenido ${usuario.nombre}`);
+                    commit('SET_ERROR_LOADED', false);
+                    commit('SET_USER_TOKEN', bodyResponse.access);
+                    commit('SET_IS_LOGIN', payload);
+                    router.push('/');
+                }
+            } catch (e) {
+                commit('SET_ERROR_LOADED', true);
+            }
+        },
+        async postRegister({ commit }, payload) {
+            try {
+                const registerResponse = await fetch(
+                    'https://backendhackatonfinal.herokuapp.com/cliente/', {
+                        method: 'POST',
+                        body: JSON.stringify(payload),
+                        headers: {
+                            'content-Type': 'application/json',
+                        },
+                    }
+                ).then((response) => response.json());
+                commit('SET_ERROR_LOADED', false);
+                alert(`${registerResponse.nombre} se ha registado satisfactoriamente`);
+                router.push('/auth/login');
+            } catch (e) {
+                commit('SET_ERROR_LOADED', true);
+            }
+        },
     },
-  },
-  modules: {},
+    modules: {},
 };
